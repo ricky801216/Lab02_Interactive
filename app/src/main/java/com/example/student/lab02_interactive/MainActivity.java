24fcd9fc15f8506b1ed7c11d30e24d365104519e
ricky801216 <ricky801216>
2016-03-11 12:47:19 +0800

    number = 2 number + -

--- app/src/main/java/com/example/student/lab02_interactive/MainActivity.java
+++ app/src/main/java/com/example/student/lab02_interactive/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.student.lab02_interactive;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+    diplay(i);
+    }
+
+    private void diplay(int number) {
+        TextView quantity = (TextView) findViewById(R.id.quantity_text_view);
+        quantity.setText(String.valueOf(number));
+        TextView priceTextView = (TextView) findViewById(R.id.pirce_text_view);
+        int price = 10;//價錢
+        int total = price * number;
+        String myString = NumberFormat.getCurrencyInstance().format(total);
+        priceTextView.setText(myString);
+    }
+
+    int i =0;
+
+    public void add(View view) {
+        a(++i);
+    }
+
+    public void back(View view) {
+        a(--i);
+    }
+
+    public void a(int nuber){
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(String.valueOf(nuber));
     }
 }
